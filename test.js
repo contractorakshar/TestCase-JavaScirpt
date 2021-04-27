54ef57df72976421b62e0564925397449fe36f58
SujalShah3234 <shahc9437@gmail.com>
2021-04-27 11:49:37 +0530

    initial commit

--- test.js
+++ test.js
@@ -2,7 +2,8 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(7), -1);
+      //assert.equal is deprecated so assert.strictEqual is used in new version
+      assert.strictEqual([1, 2, 3].indexOf(100), 1, 'Value Not Found');
     });
   });
 }); 
