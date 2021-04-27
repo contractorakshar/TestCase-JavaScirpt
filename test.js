2a6766bf289fe1db411893b0a1d3119fce4ef496
contractorakshar <contractorakshar8319@gmail.com>
2021-04-27 20:32:42 +0530

    added mocha

---
+++ test.js
@@ -0,0 +1,17 @@
+var assert = require('assert');
+describe('Array', function() {
+  describe('#indexOf()', function() {
+    it('should return -1 when the value is not present', function() {
+      assert.equal([1, 2, 3].indexOf(7), -1);
+    });
+  });
+}); 
+/*
+  Array
+    #indexOf()
+      ✓ should return -1 when the value is not present
+
+
+  1 passing
+
+*/
