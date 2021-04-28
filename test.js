8e1c57138fa5b639adbaf5c453de1f772d0fe089
contractorakshar <contractorakshar8319@gmail.com>
2021-04-28 09:30:34 +0530

    done

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
-      assert.equal([1, 2, 3].indexOf(7), -1);
+      assert.strictEqual([1, 2, 3].indexOf(7), -1);
     });
   });
 }); 
