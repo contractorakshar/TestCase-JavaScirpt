5db91856e17cf517289d7742703e4fe45d9d3d5e
SujalShah3234 <shahc9437@gmail.com>
2021-04-28 10:52:21 +0530

    basic

--- test.js
+++ test.js
@@ -1,9 +1,13 @@
+// Require the built in 'assertion' library
 var assert = require('assert');
+// Create a group of tests about Arrays
 describe('Array', function() {
+  // Within our Array group, Create a group of tests for indexOf
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      //assert.equal is deprecated so assert.strictEqual is used in new version
-      assert.strictEqual([1, 2, 3].indexOf(100), 1, 'Value Not Found');
+    // A string explanation of what we're testing
+    it('should return -1 when the value is not present', function(){
+      // Our actual test: -1 should equal indexOf(...)
+      assert.strictEqual(-1, [1,2,3].indexOf(4));
     });
   });
-}); 
+});
